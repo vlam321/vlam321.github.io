0e8be2439a0e0e4372e349da04cec46416188b9b
vlam321 <vlam321@gmail.com>
2017-11-13 23:35:24 -0800

    Bug Fixes

--- contact_form.js
+++ contact_form.js
@@ -3,6 +3,7 @@ function valOf(id) {return document.getElementById(id);}
 function submitForm() {
 	valOf("subbutton").disable = true;
 	var name = valOf("name").value;
+	var subject = valOf("subject").value;
 	var msg = valOf("msg").value;
 	var body = "";
 	var uri = "mailto:lamvc@uci.edu"	
