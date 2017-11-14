2306c6ca05f8389dd09b3c65df3f548bd1d17481
vlam321 <vlam321@gmail.com>
2017-11-13 23:06:34 -0800

    Added all files to website

---
+++ contact_form.js
@@ -0,0 +1,16 @@
+function valOf(id) {return document.getElementById(id);}
+
+function submitForm() {
+	valOf("subbutton").disable = true;
+	valOf("status").innerHTML = "Processing Form";
+	var name = valOf("name").value;
+	var subject = valOf("subject").value;
+	var msg = valOf("msg").value;
+	var body = "";
+	var uri = "mailto:lamvc@uci.edu"	
+	body += msg + "\r\n\r\n";
+	body += "Name: " + name + "\r\n";
+	uri += "?subject=" + encodeURIComponent(subject);
+	uri += "&body=" + encodeURIComponent(body);
+	window.open(uri, "_blank");
+}
